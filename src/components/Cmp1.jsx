a69aaa6ac3652cd81a855e57ee5684cf5a7f770a
upgrade07 <43794966+upgrade07@users.noreply.github.com>
2022-07-30 05:30:51 +0530

    new

--- src/components/Cmp1.jsx
+++ src/components/Cmp1.jsx
@@ -1,16 +1,55 @@
 import React, { useState } from "react";
 import { Resizable } from "re-resizable";
 export default function Cmp1() {
-   const [state, setState] = useState({ width: 400, height: 600 });
-   return (
-      <Resizable
-         style={{ marginLeft: 5,marginTop : 5, border: "1px solid black" }}
-         size={{ width: state.width, height: state.height }}
-         onResizeStop={(e, direction, ref, d) => {
-            setState({
-               width: state.width + d.width, height: state.height + d.height,});
-            }}>
-         Sample with size
-      </Resizable>
-   );
+  const [state, setState] = useState({ width: 400, height: 600 });
+  return (
+    <Resizable
+      style={{ marginLeft: 5, marginTop: 5, border: "1px solid black" }}
+      size={{ width: state.width, height: state.height }}
+      onResizeStop={(e, direction, ref, d) => {
+        setState({
+          width: state.width + d.width,
+          height: state.height + d.height,
+        });
+      }}
+    >
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
+    </Resizable>
+  );
 }
